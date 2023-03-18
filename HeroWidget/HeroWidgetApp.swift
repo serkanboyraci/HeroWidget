55038b795078899e9a91d89ea3a29c8d6c5e0297
serkanboyraci <serkanboyraci@gmail.com>
2023-03-18 15:51:42 +0300

    Initial Commit

---
+++ HeroWidget/HeroWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroWidgetApp.swift
+//  HeroWidget
+//
+//  Created by Ali serkan Boyracı  on 18.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct HeroWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
