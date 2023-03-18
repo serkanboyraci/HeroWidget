536f3504e1e1d6735d43603c6b2c9ad633ea528e
serkanboyraci <serkanboyraci@gmail.com>
2023-03-18 20:13:07 +0300

    main UI created

---
+++ WidgetHero/WidgetHeroBundle.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetHeroBundle.swift
+//  WidgetHero
+//
+//  Created by Ali serkan Boyracı  on 18.03.2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetHeroBundle: WidgetBundle {
+    var body: some Widget {
+        WidgetHero()
+        WidgetHeroLiveActivity()
+    }
+}
