a46a712da19ab56c047aa650bc9c8c8e3e431716
serkanboyraci <serkanboyraci@gmail.com>
2023-03-18 17:28:43 +0300

    created small view

---
+++ HeroWidget/CircularImageView.swift
@@ -0,0 +1,30 @@
+//
+//  CircularImageView.swift
+//  HeroWidget
+//
+//  Created by Ali serkan Boyracı  on 18.03.2023.
+//
+
+import SwiftUI
+
+struct CircularImageView: View {
+    
+    var image : Image
+    
+    var body: some View {
+        image.resizable()
+            .aspectRatio(contentMode: .fit)
+            .clipShape(Circle())
+            .overlay(Circle()
+                .stroke(Color.purple
+                        ,lineWidth: 5))
+            .shadow(radius :15)
+    }
+}
+
+struct CircularImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircularImageView(image:
+                            Image("batman"))
+    }
+}
